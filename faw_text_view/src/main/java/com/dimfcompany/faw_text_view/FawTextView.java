3d9bcabe7d429282e1d52f91a83e6a5d33722452
bios90 <bios90@mail.ru>
2019-07-13 02:21:06 +0300

    intial commit

---
+++ faw_text_view/src/main/java/com/dimfcompany/faw_text_view/FawTextView.java
@@ -0,0 +1,115 @@
+package com.dimfcompany.faw_text_view;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Typeface;
+import android.support.annotation.Nullable;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.Gravity;
+
+public class FawTextView extends AppCompatTextView
+{
+    //dummy changes
+    private Typeface typeface;
+
+    public FawTextView(Context context)
+    {
+        super(context);
+        init(null);
+    }
+
+    public FawTextView(Context context, @Nullable AttributeSet attrs)
+    {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public FawTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet attrs)
+    {
+        typeface = ResourcesCompat.getFont(getContext(), R.font.fawsolid);
+        this.setTypeface(typeface);
+        initAttributes(attrs);
+    }
+
+    private void initAttributes(@Nullable AttributeSet attrs)
+    {
+        if (attrs == null)
+        {
+            return;
+        }
+
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.FawTextView);
+
+        int icon_size = ta.getInt(R.styleable.FawTextView_icon_size, 999);
+        int text_size = ta.getDimensionPixelSize(R.styleable.FawTextView_android_textSize, 12);
+
+        switch (icon_size)
+        {
+            case 0:
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12);
+                break;
+            case 1:
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 14);
+                break;
+            case 2:
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 16);
+                break;
+            case 3:
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 18);
+                break;
+            case 4:
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+                break;
+            case 5:
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 23);
+                break;
+            case 6:
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 26);
+                break;
+            case 999:
+                this.setTextSize(TypedValue.COMPLEX_UNIT_DIP, text_size);
+        }
+
+        int gravity = ta.getInt(R.styleable.FawTextView_android_gravity, 999);
+        if (gravity == 999)
+        {
+            this.setGravity(Gravity.CENTER);
+        }
+        else
+        {
+            this.setGravity(gravity);
+        }
+
+
+        ta.recycle();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+        this.setTypeface(typeface);
+    }
+
+    private static int dp2pxInt(float dp)
+    {
+        return (int) dp2px(dp);
+    }
+
+    private static float dp2px(float dp)
+    {
+        Resources r = Resources.getSystem();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
+    }
+}
